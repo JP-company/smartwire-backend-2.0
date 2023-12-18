7084de59f395d4829368abfadf16918783d8b90e
JP-company <wjsdj2008@gmail.com>
2023-12-18 11:35:30 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# smartwire-backend2
+ 스마트 와이어 백앤드 처음부터 다시 만들기
---
+++ src/main/java/jpcompany/smartwire2/Smartwire2Application.java
@@ -0,0 +1,11 @@
+package jpcompany.smartwire2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Smartwire2Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Smartwire2Application.class, args);
+	}
+}
