4c6685421d9e999f235918211db7bcd17df6da4d
JP-company <wjsdj2008@gmail.com>
2023-12-24 17:17:27 +0900

    refactor: 에러메시지 방식 수정

--- src/main/java/jpcompany/smartwire2/dto/MemberJoinDto.java
+++ src/main/java/jpcompany/smartwire2/dto/MemberJoinDto.java
@@ -1,7 +1,6 @@
 package jpcompany.smartwire2.dto;
 
 import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,19 +9,19 @@ import lombok.ToString;
 @Setter
 @ToString
 public class MemberJoinDto {
-    @NotBlank(message = "{NotBlank.memberJoinDto.loginEmail}")
-    @Email(message = "{Email.memberJoinDto.loginEmail}")
+    @NotBlank(message = "{NotBlank.loginEmail}")
+    @Email(message = "{Email.loginEmail}")
+    @Size(max = 40, message = "{Size.email}")
     private String loginEmail;
 
-    @NotBlank(message = "{NotBlank.memberJoinDto.loginPassword}")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!?@#$%^&*+=/])[a-zA-Z\\d!?@#$%^&*+=/]{10,20}$",
-            message = "{Pattern.memberJoinDto.loginPassword}")
+    @NotBlank(message = "{NotBlank.loginPassword}")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!?@$%^&*+=])[a-zA-Z\\d!?@$%^&*+=]{10,20}$", message = "{Pattern.loginPassword}")
     private String loginPassword;
 
-    @NotBlank(message = "{NotBlank.memberJoinDto.loginPasswordVerify}")
+    @NotBlank(message = "{NotBlank.loginPasswordVerify}")
     private String loginPasswordVerify;
 
-    @NotBlank(message = "{NotBlank.memberJoinDto.companyName}")
-    @Size(min = 1, max = 20, message = "{Size.memberJoinDto.companyName}")
+    @NotBlank(message = "{NotBlank.companyName}")
+    @Size(min = 1, max = 20, message = "{Size.companyName}")
     private String companyName;
 }
