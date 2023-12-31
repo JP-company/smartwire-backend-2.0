04117e64a2bfc90b426d4695e2e153cf58fdfcf9
JP-company <wjsdj2008@gmail.com>
2023-12-31 17:16:51 +0900

    feat: JWT 토큰 생성/검증 로직

---
+++ src/main/java/jpcompany/smartwire2/common/jwt/constant/JwtConstant.java
@@ -0,0 +1,7 @@
+package jpcompany.smartwire2.common.jwt.constant;
+
+public enum JwtConstant {
+    ;
+    public static final String MAIL_TOKEN_AUTHENTICATION_SECRET = "!email@Secret!";
+    public static final Long MAIL_TOKEN_EXPIRATION_TIME = 300000L; // 5분
+}
