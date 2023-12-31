d5b0597ca735a3d446ca64ea4944eef7be364296
JP-company <wjsdj2008@gmail.com>
2023-12-31 17:19:36 +0900

    feat: 메일 인증 API

---
+++ src/main/java/jpcompany/smartwire2/controller/MemberController.java
@@ -0,0 +1,65 @@
+package jpcompany.smartwire2.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import jpcompany.smartwire2.common.error.ErrorCode;
+import jpcompany.smartwire2.common.jwt.JwtToken;
+import jpcompany.smartwire2.controller.dto.request.MemberJoinDto;
+import jpcompany.smartwire2.controller.dto.request.mapper.MemberCommandMapper;
+import jpcompany.smartwire2.controller.dto.request.validator.JoinValidator;
+import jpcompany.smartwire2.controller.dto.response.ResponseDto;
+import jpcompany.smartwire2.domain.Member;
+import jpcompany.smartwire2.service.MemberService;
+import jpcompany.smartwire2.service.dto.MemberJoinCommand;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@Controller
+public class MemberController {
+
+    private final JoinValidator joinValidator;
+    private final MemberService memberService;
+    private final JwtToken jwtToken;
+
+    @Operation(summary = "회원 가입 페이지 요청", description = "화원 가입을 요청합니다.")
+    @PostMapping("/join")
+    @ResponseBody
+    public ResponseEntity<ResponseDto> join(@Validated @RequestBody MemberJoinDto memberJoinDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            ErrorCode errorCode = ErrorCode.INVALID_INPUT;
+            return ResponseEntity.status(errorCode.getHttpStatus())
+                    .body(new ResponseDto(false, errorCode.getReason(), null));
+        }
+
+        MemberJoinCommand memberJoinCommand =
+                MemberCommandMapper.mapToMemberJoinCommand(memberJoinDto);
+        memberService.join(memberJoinCommand);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ResponseDto(true, null, null));
+    }
+
+    @GetMapping("/email_verify/{authToken}")
+    public String emailVerify(@PathVariable String authToken, Model model) {
+        Long memberId = jwtToken.extractMemberIdFromEmailAuthToken(authToken);
+        memberService.authenticateMember(memberId, Member.Role.MEMBER);
+        model.addAttribute("verified", "인증에 성공하였습니다.");
+        return "email/result";
+    }
+
+    @InitBinder
+    public void init(WebDataBinder dataBinder) {
+        dataBinder.addValidators(joinValidator);
+    }
+}
