768d829ec65f0afe8af9b0744c260a81c4f1d8a5
JP-company <wjsdj2008@gmail.com>
2024-01-05 18:52:47 +0900

    feat: jwt토큰 서비스

--- src/main/java/jpcompany/smartwire2/common/jwt/constant/JwtConstant.java
+++ src/main/java/jpcompany/smartwire2/common/jwt/constant/JwtConstant.java
@@ -3,5 +3,9 @@ package jpcompany.smartwire2.common.jwt.constant;
 public enum JwtConstant {
     ;
     public static final String MAIL_TOKEN_AUTHENTICATION_SECRET = "!email@Secret!";
-    public static final Long MAIL_TOKEN_EXPIRATION_TIME = 300000L; // 5분
+    public static final Long MAIL_TOKEN_EXPIRATION_TIME = 30000000L; // 500분
+    public static final String HEADER_STRING = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer";
+
+
 }
