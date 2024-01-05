1393fe66b4b516da145a6e517ab8919cccf1efa2
JP-company <wjsdj2008@gmail.com>
2024-01-05 19:07:31 +0900

    test: Repository 유닛 테스트 코드 변경

--- src/test/java/jpcompany/smartwire2/unit/repository/jdbctemplate/MemberRepositoryJdbcTemplateTest.java
+++ src/test/java/jpcompany/smartwire2/unit/repository/jdbctemplate/MemberRepositoryJdbcTemplateTest.java
@@ -2,6 +2,7 @@ package jpcompany.smartwire2.unit.repository.jdbctemplate;
 
 import jpcompany.smartwire2.domain.Member;
 import jpcompany.smartwire2.repository.jdbctemplate.MemberRepositoryJdbcTemplate;
+import jpcompany.smartwire2.repository.jdbctemplate.dto.MemberJoinTransfer;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,14 +31,21 @@ class MemberRepositoryJdbcTemplateTest {
     @DisplayName("정상 회원값 저장")
     void save() {
         // given
-        Member member = Member.createMember(
+        Member member = Member.initMember(
                 "wjsdj2008@naver.com",
                 "Qwertyuiop1!",
                 "JP-comapny"
         );
+        MemberJoinTransfer memberJoinTransfer = MemberJoinTransfer.builder()
+                        .loginEmail(member.getLoginEmail())
+                        .loginPassword(member.getLoginPassword())
+                        .companyName(member.getCompanyName())
+                        .role(member.getRole())
+                        .createdDateTime(member.getCreatedDateTime())
+                        .build();
 
         // when
-        Long memberId = memberRepositoryJdbcTemplate.save(member);
+        Long memberId = memberRepositoryJdbcTemplate.save(memberJoinTransfer);
 
         // then
         Assertions.assertThat(memberId).isGreaterThan(0);
@@ -47,15 +55,22 @@ class MemberRepositoryJdbcTemplateTest {
     @DisplayName("중복 이메일 저장 시 예외 발생")
     void saveDuplicatedEmail() {
         // given
-        Member member = Member.createMember(
+        Member member = Member.initMember(
                 "wjsdj2008@naver.com",
                 "Qwertyuiop1!",
                 "JP-comapny"
         );
-        memberRepositoryJdbcTemplate.save(member);
+        MemberJoinTransfer memberJoinTransfer = MemberJoinTransfer.builder()
+                .loginEmail(member.getLoginEmail())
+                .loginPassword(member.getLoginPassword())
+                .companyName(member.getCompanyName())
+                .role(member.getRole())
+                .createdDateTime(member.getCreatedDateTime())
+                .build();
+        memberRepositoryJdbcTemplate.save(memberJoinTransfer);
 
         // when, then
-        Assertions.assertThatThrownBy(() -> memberRepositoryJdbcTemplate.save(member))
+        Assertions.assertThatThrownBy(() -> memberRepositoryJdbcTemplate.save(memberJoinTransfer))
                 .isInstanceOf(DuplicateKeyException.class);
     }
 }
