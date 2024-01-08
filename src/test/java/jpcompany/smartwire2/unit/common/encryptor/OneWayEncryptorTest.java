5f7c5592cf6537e44ad3beb6ccb1f0c71b0ddaa0
JP-company <wjsdj2008@gmail.com>
2024-01-08 16:23:51 +0900

    test: 단뱡향 암호화 테스트 수정

--- src/test/java/jpcompany/smartwire2/unit/common/encryptor/OneWayEncryptorTest.java
+++ src/test/java/jpcompany/smartwire2/unit/common/encryptor/OneWayEncryptorTest.java
@@ -6,15 +6,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class OneWayEncryptorTest {
 
     @Autowired
     private OneWayEncryptor oneWayEncryptor;
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Test
     @DisplayName("단방향 암호화 같은 원문을 암호화해도 다른 암호문 반환")
@@ -32,10 +29,10 @@ class OneWayEncryptorTest {
     void encrypt2() {
         String originalMessage = "secretMessage";
 
-        String encodedMessage1 = bCryptPasswordEncoder.encode(originalMessage);
-        String encodedMessage2 = bCryptPasswordEncoder.encode(originalMessage);
+        String encodedMessage1 = oneWayEncryptor.encrypt(originalMessage);
+        String encodedMessage2 = oneWayEncryptor.encrypt(originalMessage);
 
-        Assertions.assertThat(bCryptPasswordEncoder.matches(originalMessage, encodedMessage1)).isTrue();
-        Assertions.assertThat(bCryptPasswordEncoder.matches(originalMessage, encodedMessage2)).isTrue();
+        Assertions.assertThat(oneWayEncryptor.matches(originalMessage, encodedMessage1)).isTrue();
+        Assertions.assertThat(oneWayEncryptor.matches(originalMessage, encodedMessage2)).isTrue();
     }
 }
