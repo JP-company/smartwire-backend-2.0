06fecfddbdcbb9cbb01ff79e4016c54ee2342d7a
JP-company <wjsdj2008@gmail.com>
2024-01-08 16:23:27 +0900

    fix: 이메일 보내는 사람

--- src/main/java/jpcompany/smartwire2/common/email/EmailService.java
+++ src/main/java/jpcompany/smartwire2/common/email/EmailService.java
@@ -3,6 +3,7 @@ package jpcompany.smartwire2.common.email;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -12,15 +13,15 @@ public class EmailService {
 
     private final JavaMailSender javaMailSender;
 
-//    @Value("${email.from}")
-//    String emailFrom;
+    @Value("${email.from}")
+    String emailFrom;
 
     public void sendEmail(String emailTo, String title, String content) {
         try {
             MimeMessage emailForm = javaMailSender.createMimeMessage();
             emailForm.addRecipients(MimeMessage.RecipientType.TO, emailTo);
             emailForm.setSubject(title);
-            emailForm.setFrom("emailFrom");
+            emailForm.setFrom(emailFrom);
             emailForm.setText(content, "utf-8", "html");
             javaMailSender.send(emailForm);
         } catch (MessagingException e) {
