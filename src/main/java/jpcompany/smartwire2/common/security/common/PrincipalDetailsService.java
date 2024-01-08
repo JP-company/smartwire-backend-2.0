d5ccd0116ed61737eb793319951a9796d5b6d488
JP-company <wjsdj2008@gmail.com>
2024-01-08 16:09:24 +0900

    refactor: 시큐리티 인증 예외 위치 변경

--- src/main/java/jpcompany/smartwire2/common/security/common/PrincipalDetailsService.java
+++ src/main/java/jpcompany/smartwire2/common/security/common/PrincipalDetailsService.java
@@ -14,12 +14,7 @@ public class PrincipalDetailsService implements UserDetailsService {
     private final MemberService memberService;
     @Override
     public UserDetails loadUserByUsername(String loginEmail) {
-
         Member member = memberService.findMember(loginEmail);
-
-        if (member.getRole().equals(Member.Role.EMAIL_UNAUTHORIZED)) {
-            throw new IllegalStateException("이메일 미인증 계정");
-        }
         return new PrincipalDetails(member);
     }
 }
