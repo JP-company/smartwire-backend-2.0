186061c71d397aa47bcb1524b5e21ab6b736c449
JP-company <wjsdj2008@gmail.com>
2024-01-08 16:21:08 +0900

    style: Import 삭제

--- src/test/java/jpcompany/smartwire2/integrated/MemberIntegratedTest.java
+++ src/test/java/jpcompany/smartwire2/integrated/MemberIntegratedTest.java
@@ -12,15 +12,9 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import javax.sql.DataSource;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
