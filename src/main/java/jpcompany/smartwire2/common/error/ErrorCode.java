42f6f23a97620abe34860ef669eed8fb79f85a6e
JP-company <wjsdj2008@gmail.com>
2024-01-08 15:59:19 +0900

    refactor: errorcode 관리 방식 DB에서 코드 내부로 변경

--- src/main/java/jpcompany/smartwire2/common/error/ErrorCode.java
+++ src/main/java/jpcompany/smartwire2/common/error/ErrorCode.java
@@ -1,41 +1,19 @@
 package jpcompany.smartwire2.common.error;
 
-import jakarta.annotation.PostConstruct;
-import jpcompany.smartwire2.repository.jdbctemplate.ErrorCodeRepositoryJdbcTemplate;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-import java.util.Arrays;
 
 @Getter
 public enum ErrorCode {
-    INVALID_INPUT,
-    EXIST_MEMBER
+    INVALID_INPUT("유효하지 않은 입력값", HttpStatus.BAD_REQUEST),
+    EXIST_MEMBER("이미 존재하는 계정", HttpStatus.INTERNAL_SERVER_ERROR)
     ;
 
     private String reason;
     private HttpStatus httpStatus;
 
-    @RequiredArgsConstructor
-    @Component
-    public static class ErrorMessageInjector {
-
-        private final ErrorCodeRepositoryJdbcTemplate errorCodeRepositoryJdbcTemplate;
-        @Value("${error.message.locale}")
-        private String locale;
-
-        @PostConstruct
-        public void postConstruct() {
-            Arrays.stream(ErrorCode.values())
-                    .forEach(errorCode -> // EMPTY, INVALID_PASSWORD , ...
-                            errorCodeRepositoryJdbcTemplate.findByNameAndLocale(errorCode.name(), locale)
-                                    .ifPresent(errorCodeDto -> {
-                                        errorCode.httpStatus = errorCodeDto.getHttpStatus();
-                                        errorCode.reason = errorCodeDto.getReason();
-                                    })
-                    );
-        }
+    ErrorCode(String reason, HttpStatus httpStatus) {
+        this.reason = reason;
+        this.httpStatus = httpStatus;
     }
 }
