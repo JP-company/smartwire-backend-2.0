cac1e7b658e63edbd71e63b6106a746fed504082
JP-company <wjsdj2008@gmail.com>
2024-01-08 16:05:26 +0900

    refactor: Member 상수 정리

---
+++ src/main/java/jpcompany/smartwire2/repository/jdbctemplate/constant/MemberConstantDB.java
@@ -0,0 +1,11 @@
+package jpcompany.smartwire2.repository.jdbctemplate.constant;
+
+public interface MemberConstantDB {
+    String TABLE_NAME = "members";
+    String ID = "id";
+    String LOGIN_EMAIL = "login_email";
+    String LOGIN_PASSWORD = "login_password";
+    String COMPANY_NAME = "company_name";
+    String ROLE = "role";
+    String CREATED_DATE_TIME = "created_date_time";
+}
