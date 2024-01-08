e92841d71bce24f52a77e613a09fccfb31f59f04
JP-company <wjsdj2008@gmail.com>
2024-01-08 16:15:17 +0900

    refactor: 빌더 추가

--- src/main/java/jpcompany/smartwire2/controller/dto/request/MemberLoginDto.java
+++ src/main/java/jpcompany/smartwire2/controller/dto/request/MemberLoginDto.java
@@ -1,10 +1,12 @@
 package jpcompany.smartwire2.controller.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class MemberLoginDto {
     private String loginEmail;
     private String loginPassword;
