e5d6bbdb234b966b86579a18c52ae61f3fcf7f8e
JP-company <wjsdj2008@gmail.com>
2024-01-08 16:07:12 +0900

    refactor: Jwt 토큰 생성, 추출 역할별 분리

--- src/main/java/jpcompany/smartwire2/common/jwt/JwtTokenService.java
+++ src/main/java/jpcompany/smartwire2/common/jwt/JwtTokenService.java
@@ -5,43 +5,40 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import jpcompany.smartwire2.common.jwt.constant.JwtConstant;
 import jpcompany.smartwire2.common.jwt.dto.MemberTokenDto;
-import jpcompany.smartwire2.repository.jdbctemplate.constant.MemberConstant;
+import jpcompany.smartwire2.domain.constant.MemberConstant;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class JwtTokenService {
-    public String createEmailAuthJwtToken(Long memberId) {
+    public String createEmailAuthToken(Long memberId) {
         return JWT.create()
                 .withExpiresAt(new Date(System.currentTimeMillis() + JwtConstant.MAIL_TOKEN_EXPIRATION_TIME))
                 .withClaim(MemberConstant.ID, memberId)
-                .sign(Algorithm.HMAC512(JwtConstant.MAIL_TOKEN_AUTHENTICATION_SECRET));
+                .sign(Algorithm.HMAC512(JwtConstant.MAIL_TOKEN_SECRET));
     }
 
-    public String createLoginAuthJwtToken(Long memberId, String loginEmail) {
+    public String createLoginAuthToken(Long memberId) {
         return JWT.create()
-                .withExpiresAt(new Date(System.currentTimeMillis() + JwtConstant.MAIL_TOKEN_EXPIRATION_TIME))
+                .withExpiresAt(new Date(System.currentTimeMillis() + JwtConstant.LOGIN_TOKEN_EXPIRATION_TIME))
                 .withClaim(MemberConstant.ID, memberId)
-                .withClaim(MemberConstant.LOGIN_EMAIL, loginEmail)
-                .sign(Algorithm.HMAC512(JwtConstant.MAIL_TOKEN_AUTHENTICATION_SECRET));
+                .sign(Algorithm.HMAC512(JwtConstant.LOGIN_TOKEN_SECRET));
     }
 
-    public Long extractMemberIdFrom(String authToken) {
-        return JWT.require(Algorithm.HMAC512(JwtConstant.MAIL_TOKEN_AUTHENTICATION_SECRET)).build()
-                .verify(authToken) // TokenExpiredException 유효 시간, SignatureVerificationException 시그니쳐
-                .getClaim(MemberConstant.ID)
-                .asLong();
-    }
-
-    public MemberTokenDto extractMemberTokenDtoFrom(String authToken) {
+    public Long extractMemberIdFromEmailAuthToken(String authToken) {
         DecodedJWT decodedJWT =
-                JWT.require(Algorithm.HMAC512(JwtConstant.MAIL_TOKEN_AUTHENTICATION_SECRET)).build()
+                JWT.require(Algorithm.HMAC512(JwtConstant.MAIL_TOKEN_SECRET)).build()
                 .verify(authToken); // TokenExpiredException 유효 시간, SignatureVerificationException 시그니쳐
 
-        Long memberId = decodedJWT.getClaim(MemberConstant.ID).asLong();
-        String loginEmail = decodedJWT.getClaim(MemberConstant.LOGIN_EMAIL).asString();
+        return decodedJWT.getClaim(MemberConstant.ID).asLong();
+    }
 
-        return new MemberTokenDto(memberId, loginEmail);
+    public Long extractMemberIdFromLoginAuthToken(String authToken) {
+        DecodedJWT decodedJWT =
+                JWT.require(Algorithm.HMAC512(JwtConstant.LOGIN_TOKEN_SECRET)).build()
+                        .verify(authToken); // TokenExpiredException 유효 시간, SignatureVerificationException 시그니쳐
+
+        return decodedJWT.getClaim(MemberConstant.ID).asLong();
     }
 }
--- src/main/java/jpcompany/smartwire2/common/jwt/constant/JwtConstant.java
+++ src/main/java/jpcompany/smartwire2/common/jwt/constant/JwtConstant.java
@@ -2,10 +2,11 @@ package jpcompany.smartwire2.common.jwt.constant;
 
 public enum JwtConstant {
     ;
-    public static final String MAIL_TOKEN_AUTHENTICATION_SECRET = "!email@Secret!";
-    public static final Long MAIL_TOKEN_EXPIRATION_TIME = 30000000L; // 500분
+
+    public static final String LOGIN_TOKEN_SECRET = "!login@secret!";
+    public static final String MAIL_TOKEN_SECRET = "!email@secret!";
+    public static final Long MAIL_TOKEN_EXPIRATION_TIME = 300000L; // 5분
+    public static final Long LOGIN_TOKEN_EXPIRATION_TIME = 30000000L; // 500분
     public static final String HEADER_STRING = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer";
-
-
 }
