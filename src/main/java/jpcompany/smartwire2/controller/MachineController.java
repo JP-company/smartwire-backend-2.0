bb921bde3be69d86be808dbc452d6a073c821454
JP-company <wjsdj2008@gmail.com>
2024-01-10 21:14:22 +0900

    feat: 기계 정보 페이지 API GET, 리펙토링

--- src/main/java/jpcompany/smartwire2/controller/MachineController.java
+++ src/main/java/jpcompany/smartwire2/controller/MachineController.java
@@ -1,5 +1,7 @@
 package jpcompany.smartwire2.controller;
 
+import jpcompany.smartwire2.controller.dto.request.MachineSetupDto;
+import jpcompany.smartwire2.domain.Machine;
 import jpcompany.smartwire2.service.dto.MachineSetupCommand;
 import jpcompany.smartwire2.controller.dto.response.ResponseDto;
 import jpcompany.smartwire2.domain.Member;
@@ -9,10 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,11 +25,24 @@ public class MachineController {
     @PostMapping("/machine/setup")
     public ResponseEntity<ResponseDto> setupMachines(
             @AuthenticationPrincipal Member member,
-            @Validated @RequestBody List<MachineSetupCommand> machinesForm
+            @Validated @RequestBody List<MachineSetupDto> usersMachinesForm
     ) {
-        machineService.save(machinesForm, member.getId());
+        List<MachineSetupCommand> machinesSetupForm = usersMachinesForm.stream()
+                .map(MachineSetupDto::toMachineSetupCommand)
+                .toList();
+
+        machineService.setup(machinesSetupForm, member.getId());
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new ResponseDto(true, null, null));
+    }
+
+    @GetMapping("/machine")
+    public ResponseEntity<ResponseDto> getMachines(
+            @AuthenticationPrincipal Member member
+    ) {
+        List<Machine> machines = machineService.findMachines(member.getId());
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ResponseDto(true, null, machines));
     }
 }
