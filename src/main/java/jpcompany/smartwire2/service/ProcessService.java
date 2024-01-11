55fe2bef23b6bea8cc3995740236af7422b94cf9
JP-company <wjsdj2008@gmail.com>
2024-01-11 21:54:37 +0900

    feat: Service 작업 저장 기능

---
+++ src/main/java/jpcompany/smartwire2/service/ProcessService.java
@@ -0,0 +1,19 @@
+package jpcompany.smartwire2.service;
+
+import jpcompany.smartwire2.repository.jdbctemplate.ProcessRepositoryJdbcTemplate;
+import jpcompany.smartwire2.repository.jdbctemplate.dto.ProcessSaveTransfer;
+import jpcompany.smartwire2.service.dto.ProcessSaveCommand;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProcessService {
+
+    private final ProcessRepositoryJdbcTemplate processRepositoryJdbcTemplate;
+
+    public void saveProcess(ProcessSaveCommand processSaveCommand) {
+        ProcessSaveTransfer processSaveTransfer = processSaveCommand.toProcessSaveTransfer();
+        processRepositoryJdbcTemplate.save(processSaveTransfer);
+    }
+}
