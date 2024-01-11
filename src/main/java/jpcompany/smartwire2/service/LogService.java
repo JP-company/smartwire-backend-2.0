8de8175844432c583dd2f829395dddcf277167de
JP-company <wjsdj2008@gmail.com>
2024-01-11 21:55:19 +0900

    feat: Service 로그 저장 기능

---
+++ src/main/java/jpcompany/smartwire2/service/LogService.java
@@ -0,0 +1,28 @@
+package jpcompany.smartwire2.service;
+
+import jpcompany.smartwire2.repository.jdbctemplate.LogRepositoryJdbcTemplate;
+import jpcompany.smartwire2.repository.jdbctemplate.dto.LogSaveTransfer;
+import jpcompany.smartwire2.service.dto.LogSaveCommand;
+import jpcompany.smartwire2.service.dto.ProcessSaveCommand;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogService {
+
+    private final LogRepositoryJdbcTemplate logRepositoryJdbcTemplate;
+    private final ProcessService processService;
+
+    public void saveLog(LogSaveCommand logSaveCommand, ProcessSaveCommand processSaveCommand) {
+        LogSaveTransfer logSaveTransfer = logSaveCommand.toLogSaveTransfer();
+        logRepositoryJdbcTemplate.save(logSaveTransfer);
+
+        if (logSaveCommand.isFinished()) {
+            processService.finishProcess(processSaveCommand);
+        }
+        if (processSaveCommand.isNotEmpty()) {
+            processService.saveProcess(processSaveCommand);
+        }
+    }
+}
