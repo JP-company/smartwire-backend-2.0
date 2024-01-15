b1c830d14f4adbd7d96bf55c18f851fc04614ca3
JP-company <wjsdj2008@gmail.com>
2024-01-15 11:54:07 +0900

    refactor: 예외 처리 역할 분리 To Advice

--- src/main/java/jpcompany/smartwire2/controller/MemberController.java
+++ src/main/java/jpcompany/smartwire2/controller/MemberController.java
@@ -1,7 +1,6 @@
 package jpcompany.smartwire2.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import jpcompany.smartwire2.common.error.ErrorCode;
 import jpcompany.smartwire2.controller.dto.request.MemberJoinDto;
 import jpcompany.smartwire2.controller.dto.request.validator.JoinValidator;
 import jpcompany.smartwire2.controller.dto.response.ResponseDto;
@@ -15,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
@@ -29,28 +27,18 @@ public class MemberController {
     private final JoinValidator joinValidator;
     private final MemberService memberService;
 
+
     @Operation(summary = "회원 가입 페이지 요청", description = "화원 가입을 요청합니다.")
     @PostMapping("/join")
     @ResponseBody
-    public ResponseEntity<ResponseDto> join(@Validated @RequestBody MemberJoinDto memberJoinDto, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            ErrorCode errorCode = ErrorCode.INVALID_INPUT;
-            return ResponseEntity.status(errorCode.getHttpStatus())
-                    .body(new ResponseDto(false, errorCode.getReason(), null));
-        }
-
+    public ResponseEntity<ResponseDto> join(
+            @Validated @RequestBody MemberJoinDto memberJoinDto) {
         MemberJoinCommand memberJoinCommand =
                 MemberJoinDto.toMemberJoinCommand(memberJoinDto);
         memberService.join(memberJoinCommand);
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new ResponseDto(true, null, null));
-    }
-
-    @GetMapping("/")
-    @ResponseBody
-    public String get(@AuthenticationPrincipal Member member) {
-        return member.toString();
     }
 
     @GetMapping("/info")
