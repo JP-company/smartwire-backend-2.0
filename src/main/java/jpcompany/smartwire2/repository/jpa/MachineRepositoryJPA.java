e85e8acc15f4ea4ab16648e0efeb6c52ca9933f0
JP-company <wjsdj2008@gmail.com>
2024-02-28 00:21:00 +0900

    refactor: Repository 계층 리펙토링

--- src/main/java/jpcompany/smartwire2/repository/jpa/MachineRepositoryJPA.java
+++ src/main/java/jpcompany/smartwire2/repository/jpa/MachineRepositoryJPA.java
@@ -4,13 +4,13 @@ import jakarta.persistence.EntityManager;
 import jpcompany.smartwire2.common.error.CustomException;
 import jpcompany.smartwire2.common.error.ErrorCode;
 import jpcompany.smartwire2.domain.Machine;
-import jpcompany.smartwire2.domain.Member;
 import jpcompany.smartwire2.repository.MachineRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,25 +18,37 @@ import java.util.List;
 public class MachineRepositoryJPA implements MachineRepository {
 
     private final EntityManager em;
+
     @Override
-    public Long save(Machine machine, Long memberId) {
-        Member findMember = em.find(Member.class, memberId);
-        machine.setMember(findMember);
+    public Long save(Machine machine) {
         em.persist(machine);
         return machine.getId();
     }
 
     @Override
-    public List<Machine> findAllByMemberId(Member member) {
+    public List<Machine> findAllByMemberId(Long memberId) {
         String jpql =
                 """
                 SELECT m
                 FROM Machine m
-                WHERE m.member = :member
+                WHERE m.memberId = :memberId
                 """;
         return em.createQuery(jpql, Machine.class)
-                .setParameter("member", member)
+                .setParameter("memberId", memberId)
                 .getResultList();
+    }
+
+    @Override
+    public Machine findByMemberAndMachineIdAndMachineUUID(Long memberId, Long machineId, String machineUUID) {
+        Machine findMachine = em.find(Machine.class, machineId);
+
+        if (findMachine == null) {
+            throw new CustomException(ErrorCode.INVALID_MACHINE);
+        }
+        if (Objects.equals(memberId, findMachine.getMemberId()) && findMachine.getMachineUUID().equals(machineUUID)) {
+            return findMachine;
+        }
+        throw new CustomException(ErrorCode.INVALID_MACHINE);
     }
 
     @Override
@@ -46,22 +58,16 @@ public class MachineRepositoryJPA implements MachineRepository {
     }
 
     @Override
-    public void updateMachineUUID(Member member, Long machineId, String machineUUID) {
-        Machine machine = em.find(Machine.class, machineId);
-        if (member.getId() == machine.getMember().getId()) {
-            machine.setMachineUUID(machineUUID);
+    public void updateMachineUUID(Long memberId, Long machineId, String machineUUID) {
+        Machine findMachine = em.find(Machine.class, machineId);
+
+        if (findMachine == null) {
+            throw new CustomException(ErrorCode.INVALID_MACHINE);
+        }
+        if (Objects.equals(memberId, findMachine.getMemberId())) {
+            findMachine.setMachineUUID(machineUUID);
             return;
         }
-        throw new CustomException(ErrorCode.UNDEFINED_ERROR);
-    }
-
-    @Override
-    public Machine findByMemberAndMachineIdAndMachineUUID(Member member, Long machineId, String machineUUID) {
-        Machine findMachine = em.find(Machine.class, machineId);
-        if (member.getId() == findMachine.getMember().getId() && findMachine.getMachineUUID().equals(machineUUID)) {
-            findMachine.setMember(member);
-            return findMachine;
-        }
-        throw new CustomException(ErrorCode.UNDEFINED_ERROR);
+        throw new CustomException(ErrorCode.INVALID_MACHINE);
     }
 }
